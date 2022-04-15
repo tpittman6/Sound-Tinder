1dab68afb9fc3573a06549b1669454d633e0c9f5
AAspan <d.spanrising@gmail.com>
2022-04-15 16:52:20 -0400

    lattmlchange

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\David\Desktop\SPRING  2022\Software Engineering CTW CSC 4350\soundtinder\env
+set VIRTUAL_ENV=C:\Users\David\Desktop\soundtinder-main\env
 
 if not defined PROMPT set PROMPT=$P$G
 
