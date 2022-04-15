08f73d0c0e386f67c4ec7202010ceb67f9ca09ba
Terry Pittman <terrydaniel25@gmail.com>
2022-04-14 23:02:04 -0400

    creating a more refined base code

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-## Setup Instructions
+"# SoundTinder" 
+"# soundtinder" 
+## Setup Instructions to run locally
 1. `pip3 install -r requirements.txt`
 2. Create a `.env` file in the top-level directory and enter the following as its contents:
 ```
-export TMDB_API_KEY="<YOUR API KEY>"
 export DATABASE_URL="<YOUR POSTGRESQL DB URL>"
+export SECRET_KEY="<MAKE UP A SECRET KEY>"
 ```
+3. Run `python3 routes.py`
 
-
-## To run the app
-1. Run `python3 routes.py`
-
-"# SoundTinder" 
-"# soundtinder" 
--- models.py
+++ models.py
@@ -2,17 +2,6 @@ from app import db
 from flask_login import UserMixin
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+# uncomment "db.create_all()" after creating models
 
-
-class Rating(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    rating = db.Column(db.Integer)
-    comment = db.Column(db.String(200))
-    username = db.Column(db.String(80))
-    movie_id = db.Column(db.Integer)
-
-
-db.create_all()
+#db.create_all()
