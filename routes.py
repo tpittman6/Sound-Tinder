a638b611dbd4dad3bc75e17893f4f533723c8597
Terry Pittman <terrydaniel25@gmail.com>
2022-04-14 23:41:32 -0400

    procfile and port

--- routes.py
+++ routes.py
@@ -72,6 +72,6 @@ def discover():
 if __name__ == "__main__":
     app.run(
         host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", 8080)),
+        port=int(os.getenv("PORT", 5000)),
         debug=True,
     )
