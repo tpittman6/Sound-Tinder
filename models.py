4fd637a2deca52b27b84ff93f99a6fbc6fc8a09f
Terry Pittman <terrydaniel25@gmail.com>
2022-04-19 02:12:29 -0400

    added Arist model and 'is_artist' User attribute

--- models.py
+++ models.py
@@ -9,11 +9,23 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(256), unique=True)
     name = db.Column(db.String(256))
+    is_artist = db.Column(db.Boolean)
+
 
 class OAuth(OAuthConsumerMixin, db.Model):
     provider_user_id = db.Column(db.String(256), unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship(User)
+
+
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist_name = db.Column(db.String(256))
+    spotify_artist_id = db.Column(db.String(256))
+    sample_music_clip = db.Column(db.String(256))
+    artist_genre_primary = db.Column(db.String(256))
+    artist_genre_secondary = db.Column(db.String(256))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 db.create_all()
