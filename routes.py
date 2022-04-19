f7e728bdc2cf35998da60b17f7f42061ae14c8d4
Terry Pittman <terrydaniel25@gmail.com>
2022-04-19 05:18:01 -0400

    updated profile and artist-registration routes

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from app import app, db
 import os
-from models import User, OAuth
-from flask import Flask, render_template, redirect, url_for
+from models import User, OAuth, Artist
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (LoginManager, UserMixin,
                          current_user, login_user, logout_user)
@@ -34,12 +34,12 @@ google_blueprint = make_google_blueprint(
 
 app.register_blueprint(google_blueprint)
 
-# This will be the 'landing page' -- decorate index.html with all landing page stuff
-# This will be where you login as well
+
 @app.route('/')
 def index():
     google_data = None
     user_info_endpoint = '/oauth2/v2/userinfo'
+    # if statement checks for authorization and assigns all google data to a variable
     if current_user.is_authenticated and google.authorized:
         google_data = google.get(user_info_endpoint).json()
     return render_template('home.html',
@@ -52,7 +52,7 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-
+# creates a session to 
 @oauth_authorized.connect_via(google_blueprint)
 def google_logged_in(blueprint, token):
     resp = blueprint.session.get('/oauth2/v2/userinfo')
@@ -80,33 +80,88 @@ def google_logged_in(blueprint, token):
     return False
 
 
-# This is where you will be redirected after successfully logging in
 @app.route("/home")
 def home():
-    user = dict(session)['profile']['email']
-    return render_template(
-        "home.html",
-        user=user,
-    )
+    google_data = None
+    user_info_endpoint = '/oauth2/v2/userinfo'
+    if current_user.is_authenticated and google.authorized:
+        google_data = google.get(user_info_endpoint).json()
+
+    return render_template('home.html',
+                           google_data=google_data,
+                           fetch_url=google.base_url + user_info_endpoint)
+
+    return render_template('/')
 
 
-# Profile info page
 @app.route("/profile")
 def profile():
+    google_data = None
+    user_info_endpoint = '/oauth2/v2/userinfo'
+    if current_user.is_authenticated and google.authorized:
+        google_data = google.get(user_info_endpoint).json()
+
+    if current_user.is_artist == 1:
+        print("this user is an artist")
     return render_template(
         "profile.html",
+        google_data=google_data,
+        fetch_url=google.base_url + user_info_endpoint
+        
+
     )
 
-# This is the page that the 'swiping' will be done on
-@app.route("/discover")
-def discover():
-    return render_template(
-        "discover.html",
-    )
+@app.route("/artist_registration", methods=['GET', 'POST'])
+def artist_registration():
+    google_data = None
+    user_info_endpoint = '/oauth2/v2/userinfo'
+    if current_user.is_authenticated and google.authorized:
+        google_data = google.get(user_info_endpoint).json()
+
+    user = User.query.filter_by(email=current_user.email).first()
+    already_artist = Artist.query.filter_by(user_email=current_user.email).all()
+
+    # function to check if user is already an artist
+    if already_artist:
+        flash("You're already an artist")
+        return redirect("/profile")
+    
+    # Creating a new artist model in database
+    if request.method == 'POST':
+        name = request.form['name']
+        spotify_id = request.form['spotify_id']
+        primary_genre = request.form['primary_genre']
+        secondary_genre = request.form['secondary_genre']
+
+        # Couldn't get the messages to flash, 
+        # but they do stop you from posting the form if you miss a field
+        if not name:
+            flash('Artist name is required')
+        elif not spotify_id:
+            flash('Valid Spotify ID is required')
+        elif not primary_genre:
+            flash('At least 1 Genre is required')
+        else:
+            new_artist = Artist(
+                artist_name = name,
+                spotify_artist_id = spotify_id,
+                artist_genre_primary = primary_genre,
+                artist_genre_secondary = secondary_genre,
+                user_email = current_user.email
+            )
+            
+            db.session.add(new_artist)
+            db.session.commit()
+
+            return redirect(url_for('profile'))
+
+    return render_template("artist_registration.html")
+
+
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", 5000)),
+       # host=os.getenv("IP", "0.0.0.0"),
+      #  port=int(os.getenv("PORT", 5000)),
         debug=True,
     )
