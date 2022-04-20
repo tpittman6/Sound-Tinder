59afcf0a967784090813c1b58dc3a5fd3940a86b
AAspan <93344588+AAspan@users.noreply.github.com>
2022-04-20 09:57:21 -0400

    aboutchange

--- routes.py
+++ routes.py
@@ -202,5 +202,7 @@ def artist_registration():
 
 if __name__ == "__main__":
     app.run(
-        debug=True
+        debug=True,
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", 5000)),
     )
