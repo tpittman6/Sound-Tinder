ceb233528a0032dbbb3920edcd11275e1d219ce4
AAspan <d.spanrising@gmail.com>
2022-04-21 01:59:43 -0400

    latestcsshtml

--- routes.py
+++ routes.py
@@ -127,21 +127,19 @@ def home():
     )
 
 # This is the page that the 'swiping' will be done on
+# search page
 @app.route("/spinder")
 def spinder():
     google_data = None
     user_info_endpoint = '/oauth2/v2/userinfo'
     if current_user.is_authenticated and google.authorized:
         google_data = google.get(user_info_endpoint).json()
-    return render_template('index.html',
+    return render_template('spinder.html',
             google_data=google_data,
             fetch_url=google.base_url + user_info_endpoint)
 
     return render_template(
-        "index.html",
-    )
-    return render_template(
-        "index.html",
+        "spinder.html",
     )
 
 @app.route("/artist_registration", methods=['GET', 'POST'])
