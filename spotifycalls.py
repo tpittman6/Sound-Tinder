f87c55f80d40aa0cafd89dc95dc6aab668f8bd0e
KwakuFlowers <kwakuflowers@gmail.com>
2022-04-22 09:32:02 -0400

    comment out testing parts

--- spotifycalls.py
+++ spotifycalls.py
@@ -1,5 +1,6 @@
 import os
 import string
+import app
 from urllib import response
 from dotenv import find_dotenv, load_dotenv
 import requests
@@ -14,7 +15,8 @@ api_token_url = "https://accounts.spotify.com/api/token"
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-
+# all comments below are just tests to see what went wrong with the API call.
+# UNIT TESTING STILL REQUIRED
 def authorize():
     DATA = {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -26,22 +28,22 @@ def authorize():
     )
     reqkey = authkeydata.json()
     token = reqkey.get("access_token")
-    print(token)
+    # print(token)
     return f"{token}"
 
 
 def artist_basic_info(artistID):
     ART_URL = api_url_artist + artistID
-    print("1")
+    # print("1")
     headers = {
         "Authorization": f"Bearer {authorize()}",
         "Accept": "application/json",
         "Content-Type": "application/json",
     }
-    print("2")
+    # print("2")
     res = requests.get(ART_URL, headers=headers)
     art_data = res.json()
-    print(art_data)
+    # print(art_data)
 
 
 artist_basic_info("5f7VJjfbwm532GiveGC0ZK")
